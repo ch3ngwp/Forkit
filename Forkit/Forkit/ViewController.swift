e031d93f81165fa63f731c5107d01110340a0575
ch3ngwp <ch3ngwp@gmail.com>
2019-09-28 15:30:04 -0700

    first commit

---
+++ Forkit/Forkit/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Forkit
+//
+//  Created by 笨蛋 on 9/28/19.
+//  Copyright © 2019 Forkit Inc. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
